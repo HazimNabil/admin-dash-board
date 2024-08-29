2d246b08b4dc086def6c9ae79059b22c1fae6aef
HazimNabil <HazemNabil95@outlook.com>
2024-08-29 22:27:01 +0200

    Create dash board view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dash_board/views/dash_board_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,11 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Placeholder(),
+      home: DashBoardView(),
     );
   }
 }

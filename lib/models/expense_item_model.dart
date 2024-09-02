5e8bceff102b3ee5ad31347172d2d80267211c58
HazimNabil <HazemNabil95@outlook.com>
2024-09-02 22:22:00 +0200

    Create expense item model

---
+++ lib/models/expense_item_model.dart
@@ -0,0 +1,10 @@
+class ExpenseItemModel {
+  final String icon, title, date, price;
+
+  ExpenseItemModel({
+    required this.icon,
+    required this.title,
+    required this.date,
+    required this.price,
+  });
+}

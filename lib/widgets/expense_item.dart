5917dc65b30d4ca6e3513930e7e795f5df8c2bc9
HazimNabil <HazemNabil95@outlook.com>
2024-09-02 23:58:40 +0200

    Add expense item info

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -1,4 +1,5 @@
 import 'package:dash_board/models/expense_item_model.dart';
+import 'package:dash_board/utils/styles.dart';
 import 'package:dash_board/widgets/expense_item_header.dart';
 import 'package:flutter/material.dart';
 
@@ -15,9 +16,25 @@ class ExpenseItem extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(12),
       ),
-      child:  Column(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ExpenseItemHeader(icon: itemModel.icon),
+          const SizedBox(height: 34),
+          Text(
+            itemModel.title,
+            style: Styles.styleSemiBold16,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            itemModel.date,
+            style: Styles.styleRegular14,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            itemModel.price,
+            style: Styles.styleSemiBold24,
+          ),
         ],
       ),
     );

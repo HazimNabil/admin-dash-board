64206f966849d77ba008e3adc29bda9b70b28197
HazimNabil <HazemNabil95@outlook.com>
2024-09-03 02:53:29 +0200

    Refactor custom background container

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -2,18 +2,16 @@ import 'package:dash_board/widgets/all_expenses_header.dart';
 import 'package:dash_board/widgets/all_expenses_items.dart';
 import 'package:flutter/material.dart';
 
+import 'custom_background_container.dart';
+
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      padding: 20,
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AllExpensesHeader(),
@@ -24,3 +22,4 @@ class AllExpenses extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/widgets/custom_background_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomBackgroundContainer extends StatelessWidget {
+  final Widget child;
+  final double padding;
+
+  const CustomBackgroundContainer({
+    super.key,
+    required this.child,
+    required this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: child,
+    );
+  }
+}

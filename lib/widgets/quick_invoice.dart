aceb90db8dcc6b3bb0afed9267792cc0907e7cca
HazimNabil <HazemNabil95@outlook.com>
2024-09-04 02:11:41 +0200

    Create quick invoice form

--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -1,6 +1,6 @@
 import 'package:dash_board/widgets/custom_background_container.dart';
+import 'package:dash_board/widgets/quick_invoice_form.dart';
 import 'package:dash_board/widgets/quick_invoice_header.dart';
-import 'package:dash_board/widgets/title_text_field.dart';
 import 'package:flutter/material.dart';
 
 import 'latest_transaction.dart';
@@ -18,10 +18,7 @@ class QuickInvoice extends StatelessWidget {
           SizedBox(height: 24),
           LatestTransaction(),
           Divider(height: 48),
-          TitleTextField(
-            title: 'Customer name',
-            hint: 'Type customer name',
-          ),
+          QuickInvoiceForm(),
         ],
       ),
     );
---
+++ lib/widgets/quick_invoice_form.dart
@@ -0,0 +1,49 @@
+import 'package:dash_board/widgets/title_text_field.dart';
+import 'package:flutter/material.dart';
+
+class QuickInvoiceForm extends StatelessWidget {
+  const QuickInvoiceForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TitleTextField(
+                title: 'Customer name',
+                hint: 'Type customer name',
+              ),
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: TitleTextField(
+                title: 'Customer Email',
+                hint: 'Type customer email',
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 24),
+        Row(
+          children: [
+            Expanded(
+              child: TitleTextField(
+                title: 'Item name',
+                hint: 'Type item name',
+              ),
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: TitleTextField(
+                title: 'Item mount',
+                hint: 'USD',
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

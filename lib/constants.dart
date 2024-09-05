60a860ec950850bb5bb461e8be86b8f42f18c121
HazimNabil <HazemNabil95@outlook.com>
2024-09-05 21:49:26 +0200

    Add main colors to constants file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF4EB7F2);
+const kSecondaryColor = Color(0xFF064061);
+const kBackgroundColor = Color(0xFFF7F9FA);
+
 const kTabletBreakpoint = 600;
 const kDesktopBreakpoint = 900;

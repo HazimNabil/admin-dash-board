1bdad179606c7f1209f7ca68670070e68925083a
HazimNabil <HazemNabil95@outlook.com>
2024-09-06 03:23:10 +0200

    Create income chart model

---
+++ lib/models/income_chart_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IncomeChartModel {
+  final double value;
+  final Color color;
+  final double radius;
+
+  const IncomeChartModel({
+    required this.value,
+    required this.color,
+    this.radius = 20,
+  });
+}

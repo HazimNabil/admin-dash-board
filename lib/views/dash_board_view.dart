aa18f437176d8c75ed3670058cb515baff49219c
HazimNabil <HazemNabil95@outlook.com>
2024-09-07 01:08:12 +0200

    Add app bar to mobile layout

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,4 +1,6 @@
+import 'package:dash_board/constants.dart';
 import 'package:dash_board/widgets/adaptive_layout.dart';
+import 'package:dash_board/widgets/custom_drawer.dart';
 import 'package:dash_board/widgets/mobile_layout.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +13,32 @@ class DashBoardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(context),
+      drawer: buildDrawer(context),
       body: AdaptiveLayout(
         mobileLayout: (context) => const MobileLayout(),
         tabletLayout: (context) => const TabletLayout(),
         desktopLayout: (context) => const DesktopLayout(),
       ),
     );
+  }
+
+  CustomDrawer? buildDrawer(BuildContext context) {
+    if (MediaQuery.sizeOf(context).width < kTabletBreakpoint) {
+      return const CustomDrawer();
+    } else {
+      return null;
+    }
+  }
+
+  AppBar? buildAppBar(BuildContext context) {
+    if (MediaQuery.sizeOf(context).width < kTabletBreakpoint) {
+      return AppBar(
+        elevation: 0,
+        backgroundColor: kPrimaryColor,
+      );
+    } else {
+      return null;
+    }
   }
 }

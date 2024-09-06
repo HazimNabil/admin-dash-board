7654cc4ec37ff08751f14a941dfd4dba19bbd71c
HazimNabil <HazemNabil95@outlook.com>
2024-09-06 21:40:48 +0200

    Create section details model

---
+++ lib/models/section_details_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SectionDetailsModel {
+  final Color color;
+  final String title, percent;
+
+  const SectionDetailsModel({
+    required this.color,
+    required this.title,
+    required this.percent,
+  });
+}
